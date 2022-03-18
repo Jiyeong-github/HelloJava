e894848832aec6f58d296ae9f72d157b96f2292d
Jiyeong-github <thisisjiyeong@yu.ac.kr>
2022-03-18 17:53:16 +0900

    XML Parsing : 이상하게 짠 건 확실;

--- src/main/java/ParsingTest/HashmapParsing.java
+++ src/main/java/ParsingTest/HashmapParsing.java
@@ -14,17 +14,16 @@ import java.util.*;
 public class HashmapParsing {
 
     static Document document = null;
-    Map<String, String> map = new HashMap<>();
+    static Map<String, Object> map = new HashMap<>();
 
     public static void main(String[] args) {
-
         try {
             File file = new File(Paths.get("").toAbsolutePath() + "/src/main/resources/xml/partner.xml");
             File NewFile = new File(Paths.get("").toAbsolutePath() + "/src/main/resources/xml/partner_new.xml");
             //파일 읽기
             DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder builder = builderFactory.newDocumentBuilder();
-            ;
+
             Document docm = builder.parse(file);
             Document doc = builder.parse(NewFile);
             docm.getDocumentElement().normalize();
@@ -36,35 +35,52 @@ public class HashmapParsing {
             NodeList nodeList = doc.getElementsByTagName("host");
             NodeList Listnode = docm.getElementsByTagName("host");
 
+            setNode(nodeList);
+            setNode(Listnode);
+            System.out.println(map);
 
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+    //같은 node가 있다면 새 node로 replace (2개)
+    //새로운 node는 put (1개)
+    //기존 node들은 냅두기(중복되는 거 많음)
+
+
     private static void setNode(NodeList nodeList) {
         if (document == null) return;
+        //root 구하기
+        Element root = document.getDocumentElement();
+        //자식 노드 목록
+        NodeList ChildNode = document.getDocumentElement().getChildNodes();
+
         for (int i = 0; i < nodeList.getLength(); i++) {
             if (nodeList.item(i).getNodeName().equals("host")) {
+                for(int j=0; j<ChildNode.getLength(); j++){
+                    Node node = ChildNode.item(j);
+                    if(node.getFirstChild().isEqualNode(root)){
+                        Element element = (Element)node;
+                        String NodeName = element.getNodeName();
+
+                        node.removeChild(root);
+                        node.appendChild(root);
+                        System.out.println(NodeName);
+                    }
+                    System.out.println(node);
+                }//2nd for
+
                 Node node = nodeList.item(i);
-                //자식 노드 목록
-                NodeList ChildNode = document.getDocumentElement().getChildNodes();
-                Element element = (Element)node;
-                String NodeName = element.getNodeName();
-                String NodeThing = element.getChildNodes().item(i).getNodeValue();
-
-                if(NodeName.equals("alias")){
-                    NodeThing.replace(ChildNode.);
+                Element element = (Element) node;
+                //GAC#01
+                if(!element.getAttribute("alias").equals("")){
+                    map.put("alias", element.getAttribute("alias"));
                 }
-                //같은 node가 있다면 새 node로 replace (2개)
-                //새로운 node는 put (1개)
-                //기존 node들은 냅두기(중복되는 거 많음)
-                if(node.getAttributes().getNamedItem("alias").getNodeValue().equals(node.getAttributes().getNamedItem("alias").getNodeValue())){
-
-                }else if(){
-
+                if(element.getAttribute("alias").equals("alias")){
+                    map.remove("alias",element.getAttribute("alias"));
+                    map.putIfAbsent("alias",element.getAttribute("alias"));
                 }
-
             }
         }
     }
